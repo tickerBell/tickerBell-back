422ddaaa447e079a211819bb4e3d8170dbec4e85
dgjinsu <rlawlstn3206@naver.com>
2023-09-20 23:20:20 +0900

    first commit

---
+++ src/main/java/com/tickerBell/TickerBellApplication.java
@@ -0,0 +1,13 @@
+package com.tickerBell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickerBellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TickerBellApplication.class, args);
+	}
+
+}
