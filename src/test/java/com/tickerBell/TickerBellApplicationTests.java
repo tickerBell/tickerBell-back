5a79cf0361b4a99a6b94ac4c99afc6d9ba63f144
dgjinsu <rlawlstn3206@naver.com>
2023-09-21 20:12:49 +0900

    fixed: 기본 test 코드 제거 후 ci/cd 시도

--- src/test/java/com/tickerBell/TickerBellApplicationTests.java
+++ src/test/java/com/tickerBell/TickerBellApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TickerBellApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
