67aeff52467c5650d7c44b8b456aac88b8dce432
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-09-22 19:19:29 +0900

    Reactor: 사용자 Id를 이용한 회원객체 조회 findByUsername 추가

--- src/main/java/com/tickerBell/domain/member/repository/MemberRepository.java
+++ src/main/java/com/tickerBell/domain/member/repository/MemberRepository.java
@@ -4,4 +4,6 @@ import com.tickerBell.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Member findByUsername(String username); // 사용자 Id를 이용해 회원객체 조회
 }
