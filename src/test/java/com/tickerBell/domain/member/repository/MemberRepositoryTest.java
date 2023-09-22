e8afbe2b2fa77e5e53f63e9387efb34cd507c830
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-09-22 19:20:03 +0900

    Reactor: @BeforeEach 를 이용한 초기 데이터 설정, findByUsernameTest 추가

--- src/test/java/com/tickerBell/domain/member/repository/MemberRepositoryTest.java
+++ src/test/java/com/tickerBell/domain/member/repository/MemberRepositoryTest.java
@@ -3,6 +3,7 @@ package com.tickerBell.domain.member.repository;
 import com.tickerBell.domain.member.entity.AuthProvider;
 import com.tickerBell.domain.member.entity.Member;
 import com.tickerBell.domain.member.entity.Role;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,18 @@ class MemberRepositoryTest {
 
     @Autowired
     private MemberRepository memberRepository;
+
+    @BeforeEach
+    public void initData() {
+        Member initMember = Member.builder()
+                .username("initUsername")
+                .password("initPassword")
+                .phone("initPhone")
+                .role(Role.ROLE_REGISTRANT)
+                .authProvider(AuthProvider.KAKAO)
+                .build();
+        memberRepository.save(initMember);
+    }
 
     @Test
     @DisplayName("회원 저장 테스트")
@@ -40,5 +53,17 @@ class MemberRepositoryTest {
         assertThat(savedMember.getRole().getDeclaringClass()).isEqualTo(member.getRole().getDeclaringClass());
         assertThat(savedMember.getAuthProvider()).isEqualTo(member.getAuthProvider());
         assertThat(savedMember.getAuthProvider().getDeclaringClass()).isEqualTo(member.getAuthProvider().getDeclaringClass());
+    }
+
+    @Test
+    void findByUsernameTest() {
+        // given
+        String username = "initUsername";
+
+        // when
+        Member findMember = memberRepository.findByUsername(username);
+
+        // then
+        assertThat(findMember.getUsername()).isEqualTo(username);
     }
 }
