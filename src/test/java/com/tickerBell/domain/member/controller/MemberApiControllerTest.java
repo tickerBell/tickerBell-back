af66c99296ada6fa6ef150350313413424b5a73e
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-09-23 11:42:23 +0900

    feat: joinMember 메서드의 checkRole 분기를 테스트하기 위한 코드 작성

--- src/test/java/com/tickerBell/domain/member/controller/MemberApiControllerTest.java
+++ src/test/java/com/tickerBell/domain/member/controller/MemberApiControllerTest.java
@@ -2,6 +2,9 @@ package com.tickerBell.domain.member.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tickerBell.domain.member.dtos.JoinMemberRequest;
+import com.tickerBell.domain.member.entity.Member;
+import com.tickerBell.domain.member.entity.Role;
+import com.tickerBell.domain.member.repository.MemberRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -10,6 +13,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -20,6 +24,8 @@ class MemberApiControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private MemberRepository memberRepository;
     private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Test
@@ -41,5 +47,34 @@ class MemberApiControllerTest {
         perform
                 .andExpect(status().isOk())
                 .andExpect(content().string("회원가입이 완료되었습니다."));
+    }
+
+    @Test
+    void checkRoleTest() throws Exception {
+        // given
+        JoinMemberRequest joinMemberRequest = new JoinMemberRequest();
+        joinMemberRequest.setPassword("pass123");
+        Boolean isRegistration = true;
+
+        for (int i = 0; i < 2; i++) {
+            joinMemberRequest.setUsername("username" + i);
+            joinMemberRequest.setIsRegistration(isRegistration);
+
+            // when
+            ResultActions perform = mockMvc.perform(post("/api/members")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(joinMemberRequest)))
+                    .andExpect(status().isOk())
+                    .andExpect(content().string("회원가입이 완료되었습니다."));
+
+            // then
+            Member findMember = memberRepository.findByUsername("username" + i);
+            if (isRegistration) {
+                assertThat(findMember.getRole()).isEqualTo(Role.ROLE_REGISTRANT);
+            } else {
+                assertThat(findMember.getRole()).isEqualTo(Role.ROLE_USER);
+            }
+            isRegistration = !isRegistration;
+        }
     }
 }
