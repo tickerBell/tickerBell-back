d724e6d873773b3425369eb10167cdb34c3d122e
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-09-23 15:03:14 +0900

    feat: login 테스트 코드, 트렌젝션과 함께 초기 회원객체 저장코드 작성

--- src/test/java/com/tickerBell/domain/member/controller/MemberApiControllerTest.java
+++ src/test/java/com/tickerBell/domain/member/controller/MemberApiControllerTest.java
@@ -5,6 +5,10 @@ import com.tickerBell.domain.member.dtos.JoinMemberRequest;
 import com.tickerBell.domain.member.entity.Member;
 import com.tickerBell.domain.member.entity.Role;
 import com.tickerBell.domain.member.repository.MemberRepository;
+import com.tickerBell.domain.member.service.MemberService;
+import com.tickerBell.global.security.dtos.LoginDto;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -12,11 +16,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -25,8 +31,25 @@ class MemberApiControllerTest {
     @Autowired
     private MockMvc mockMvc;
     @Autowired
+    private MemberService memberService;
+    @Autowired
     private MemberRepository memberRepository;
+    @Autowired
+    private PlatformTransactionManager transactionManager;
+    private TransactionStatus transactionStatus;
     private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @BeforeEach
+    public void setup() {
+        transactionStatus = transactionManager.getTransaction(new DefaultTransactionDefinition());
+
+        memberService.join("testUsername", "testPassword1!", "testPhone", "testEmail", Role.ROLE_USER, null);
+    }
+
+    @AfterEach
+    void afterTest() {
+        transactionManager.rollback(transactionStatus);
+    }
 
     @Test
     void joinMemberTest() throws Exception {
@@ -76,5 +99,25 @@ class MemberApiControllerTest {
             }
             isRegistration = !isRegistration;
         }
+    }
+
+    @Test
+    void loginTest() throws Exception {
+        // given
+        String username = "testUsername";
+        String password = "testPassword1!";
+        LoginDto loginDto = new LoginDto();
+        loginDto.setUsername(username);
+        loginDto.setPassword(password);
+
+        // when
+        ResultActions perform = mockMvc.perform(post("/api/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(loginDto)));
+
+        // then
+        perform.andExpect(status().isOk())
+                .andExpect(jsonPath("$.accessToken").isNotEmpty())
+                .andExpect(jsonPath("$.refreshToken").isNotEmpty());
     }
 }
