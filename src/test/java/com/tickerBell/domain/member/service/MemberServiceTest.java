5474195ba6cee1c586013b0f70893b0f1aa91347
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-09-26 14:54:19 +0900

    Feat: regenerateToken 메서드 성공 테스트 추가

--- src/test/java/com/tickerBell/domain/member/service/MemberServiceTest.java
+++ src/test/java/com/tickerBell/domain/member/service/MemberServiceTest.java
@@ -1,21 +1,28 @@
 package com.tickerBell.domain.member.service;
 
+import com.tickerBell.domain.member.dtos.LoginResponse;
+import com.tickerBell.domain.member.dtos.RefreshTokenRequest;
 import com.tickerBell.domain.member.entity.AuthProvider;
 import com.tickerBell.domain.member.entity.Member;
 import com.tickerBell.domain.member.entity.Role;
 import com.tickerBell.domain.member.repository.MemberRepository;
 import com.tickerBell.global.exception.CustomException;
-import com.tickerBell.global.exception.ErrorCode;
+import com.tickerBell.global.security.token.JwtTokenProvider;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
@@ -29,6 +36,12 @@ class MemberServiceTest {
     private MemberRepository memberRepository;
     @Mock
     private PasswordEncoder passwordEncoder;
+    @Mock
+    private JwtTokenProvider jwtTokenProvider;
+    @Mock
+    private RedisTemplate<String, String> redisTemplate;
+    @Mock
+    private ValueOperations valueOperations;
 
     @Test
     @DisplayName("회원가입 테스트")
@@ -76,5 +89,40 @@ class MemberServiceTest {
         verify(memberRepository, times(1)).findByUsername(username);
         verifyNoInteractions(passwordEncoder);
         verifyNoMoreInteractions(memberRepository);
+    }
+
+    @Test
+    @DisplayName("토큰 재발급 성공 테스트")
+    void regenerateTokenTest() {
+        // given
+        RefreshTokenRequest refreshTokenRequest = new RefreshTokenRequest();
+        String mockRefreshToken = "mockRefreshToken";
+        String mockUsername = "mockUsername";
+        refreshTokenRequest.setRefreshToken(mockRefreshToken);
+        Claims mockClaim = Jwts.claims();
+        mockClaim.put("username", mockUsername);
+
+        // stub
+        when(jwtTokenProvider.isExpiration(refreshTokenRequest.getRefreshToken())).thenReturn(false);
+        when(jwtTokenProvider.get(refreshTokenRequest.getRefreshToken())).thenReturn(mockClaim);
+        when(redisTemplate.opsForValue()).thenReturn(valueOperations);
+        when(valueOperations.get(mockUsername)).thenReturn(refreshTokenRequest.getRefreshToken());
+        when(jwtTokenProvider.createRefreshToken(mockUsername)).thenReturn("newRefreshToken");
+        when(jwtTokenProvider.createAccessToken(mockUsername)).thenReturn("mockAccessToken");
+
+        // when
+        LoginResponse result = memberService.regenerateToken(refreshTokenRequest);
+
+        // then
+        verify(jwtTokenProvider, times(1)).isExpiration(refreshTokenRequest.getRefreshToken());
+        verify(jwtTokenProvider, times(1)).get(refreshTokenRequest.getRefreshToken());
+        verify(redisTemplate, times(1)).opsForValue();
+        verify(valueOperations, times(1)).get(mockUsername);
+        verify(jwtTokenProvider, times(1)).createRefreshToken(mockUsername);
+        verify(jwtTokenProvider, times(1)).createAccessToken(mockUsername);
+        verify(jwtTokenProvider, times(1)).saveRefreshTokenInRedis(mockUsername, "newRefreshToken");
+        assertThat(result).isNotNull();
+        assertThat("newRefreshToken").isEqualTo(result.getRefreshToken());
+        assertThat("mockAccessToken").isEqualTo(result.getAccessToken());
     }
 }
