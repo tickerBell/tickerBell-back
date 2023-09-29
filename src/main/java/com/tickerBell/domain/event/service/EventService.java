8a78d256953670459ad12a07cbbe8c78826f2f6e
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-09-29 13:55:35 +0900

    Feat: Event 엔티티에 Member 연관관계 설정으로 인해 Service 계층 로직을 변경하였습니다.

--- src/main/java/com/tickerBell/domain/event/service/EventService.java
+++ src/main/java/com/tickerBell/domain/event/service/EventService.java
@@ -3,5 +3,5 @@ package com.tickerBell.domain.event.service;
 import java.time.LocalDateTime;
 
 public interface EventService {
-    Long saveEvent(String name, LocalDateTime startEvent, LocalDateTime endEvent, Integer normalPrice, Integer premiumPrice, Float saleDegree, String casting, String host, String place, Integer age);
+    Long saveEvent(Long memberId, String name, LocalDateTime startEvent, LocalDateTime endEvent, Integer normalPrice, Integer premiumPrice, Float saleDegree, String casting, String host, String place, Integer age);
 }
--- src/main/java/com/tickerBell/domain/event/service/EventServiceImpl.java
+++ src/main/java/com/tickerBell/domain/event/service/EventServiceImpl.java
@@ -2,6 +2,10 @@ package com.tickerBell.domain.event.service;
 
 import com.tickerBell.domain.event.entity.Event;
 import com.tickerBell.domain.event.repository.EventRepository;
+import com.tickerBell.domain.member.entity.Member;
+import com.tickerBell.domain.member.repository.MemberRepository;
+import com.tickerBell.global.exception.CustomException;
+import com.tickerBell.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -17,8 +21,14 @@ public class EventServiceImpl implements EventService {
 
     private final Integer TOTALSEAT = 60; // 총 좌석 수 60으로 고정
     private final EventRepository eventRepository;
+    private final MemberRepository memberRepository;
 
-    public Long saveEvent(String name, LocalDateTime startEvent, LocalDateTime endEvent, Integer normalPrice, Integer premiumPrice, Float saleDegree, String casting, String host, String place, Integer age) {
+    public Long saveEvent(Long memberId, String name, LocalDateTime startEvent, LocalDateTime endEvent, Integer normalPrice, Integer premiumPrice, Float saleDegree, String casting, String host, String place, Integer age) {
+
+        Member findMember = memberRepository.findById(memberId).orElseThrow(
+                () -> new CustomException(ErrorCode.MEMBER_NOT_FOUND)
+        );
+
         Event event = Event.builder()
                 .name(name)
                 .startEvent(startEvent)
@@ -31,6 +41,7 @@ public class EventServiceImpl implements EventService {
                 .remainSeat(TOTALSEAT) // remainSeat 는 등록 시 totalSeat 와 같다고 구현
                 .host(host)
                 .place(place)
+                .member(findMember)
                 .build();
 
         Event savedEvent = eventRepository.save(event);
