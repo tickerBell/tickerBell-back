ba8c9aa26fc7abf57bb3424197b20319f04a9bf0
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-09-29 09:37:48 +0900

    Test: EventService 의 saveEvent 메서드 단위 테스트 구현

---
+++ src/test/java/com/tickerBell/domain/event/service/EventServiceTest.java
@@ -0,0 +1,51 @@
+package com.tickerBell.domain.event.service;
+
+import com.tickerBell.domain.event.entity.Event;
+import com.tickerBell.domain.event.repository.EventRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class EventServiceTest {
+
+    @InjectMocks
+    private EventServiceImpl eventService;
+    @Mock
+    private EventRepository eventRepository;
+
+    @Test
+    @DisplayName("이벤트 저장 테스트")
+    void saveEventTest() {
+        // given
+        String name = "mockName";
+        LocalDateTime startEvent = LocalDateTime.now();
+        LocalDateTime endEvent = LocalDateTime.now();
+        Integer normalPrice = 100;
+        Integer premiumPrice = 1000;
+        Float saleDegree = 0.0F;
+        String casting = "mockCasting";
+        Integer totalSeat = 60;
+        String host = "mockHost";
+        String place = "mockPlace";
+        Integer seatXBound = 60;
+        Integer seatYBound = 60;
+
+        // stub
+        when(eventRepository.save(any(Event.class))).thenReturn(Event.builder().build());
+
+        // when
+        Long savedEventId = eventService.saveEvent(name, startEvent, endEvent, normalPrice, premiumPrice, saleDegree, casting, totalSeat, host, place, seatXBound, seatYBound);
+
+        // then
+        verify(eventRepository, times(1)).save(any(Event.class));
+    }
+}
