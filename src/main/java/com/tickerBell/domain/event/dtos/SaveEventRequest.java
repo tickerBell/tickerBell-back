595b872f7eaaa93d185b78bbc36df89ae18f8b14
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-09-29 15:00:34 +0900

    Feat: 이벤트 등록 Dto 객체 구현

---
+++ src/main/java/com/tickerBell/domain/event/dtos/SaveEventRequest.java
@@ -0,0 +1,45 @@
+package com.tickerBell.domain.event.dtos;
+
+import com.tickerBell.domain.category.entity.Categories;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+public class SaveEventRequest {
+
+    @Schema(description = "이벤트 이름", example = "eventName")
+    private String name;
+    @Schema(description = "이벤트 시작 시간", example = "2023-09-29T14:30:00")
+    private LocalDateTime startEvent;
+    @Schema(description = "이벤트 시작 시간", example = "2023-09-30T14:30:00")
+    private LocalDateTime endEvent;
+    @Schema(description = "일반석 가격", example = "10000")
+    private Integer normalPrice;
+    @Schema(description = "특수석 가격", example = "15000")
+    private Integer premiumPrice;
+    @Schema(description = "자체할인", example = "1000")
+    private Float saleDegree;
+    @Schema(description = "배우", example = "출연자1, 출연자2")
+    private String casting;
+    @Schema(description = "주최자", example = "host")
+    private String host;
+    @Schema(description = "이벤트 장소", example = "서울특별시")
+    private String place;
+    @Schema(description = "연령제한", example = "18")
+    private Integer age;
+    @Schema(description = "A 좌석 특별석 여부", example = "true")
+    private Boolean isSpecialA;
+    @Schema(description = "B 좌석 특별석 여부", example = "true")
+    private Boolean isSpecialB;
+    @Schema(description = "C 좌석 특별석 여부", example = "true")
+    private Boolean isSpecialC;
+    @Schema(description = "카테고리", example = "[\"MUSICAL\", \"CONCERT\"]")
+    private List<Categories> categories;
+    @Schema(description = "태그", example = "[\"tag1\", \"tag2\"]")
+    private List<String> tags;
+}
