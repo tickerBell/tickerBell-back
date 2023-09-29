47fc00dd16dd0339ada923ef0f711afdd46951d2
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-09-29 09:36:46 +0900

    Feat: EventService 구현체와 saveEvent 메서드 구현

--- src/main/java/com/tickerBell/domain/event/service/EventService.java
+++ src/main/java/com/tickerBell/domain/event/service/EventService.java
@@ -1,4 +1,7 @@
 package com.tickerBell.domain.event.service;
 
+import java.time.LocalDateTime;
+
 public interface EventService {
+    public Long saveEvent(String name, LocalDateTime startEvent, LocalDateTime endEvent, Integer normalPrice, Integer premiumPrice, Float saleDegree, String casting, Integer totalSeat, String host, String place, Integer seatXBound, Integer seatYBound);
 }
---
+++ src/main/java/com/tickerBell/domain/event/service/EventServiceImpl.java
@@ -0,0 +1,41 @@
+package com.tickerBell.domain.event.service;
+
+import com.tickerBell.domain.event.entity.Event;
+import com.tickerBell.domain.event.repository.EventRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class EventServiceImpl implements EventService {
+
+    private EventRepository eventRepository;
+
+    public Long saveEvent(String name, LocalDateTime startEvent, LocalDateTime endEvent, Integer normalPrice, Integer premiumPrice, Float saleDegree, String casting, Integer totalSeat, String host, String place, Integer seatXBound, Integer seatYBound) {
+        Event event = Event.builder()
+                .name(name)
+                .startEvent(startEvent)
+                .endEvent(endEvent)
+                .normalPrice(normalPrice)
+                .premiumPrice(premiumPrice)
+                .saleDegree(saleDegree)
+                .casting(casting)
+                .totalSeat(totalSeat)
+                .remainSeat(totalSeat) // remainSeat 는 등록 시 totalSeat 와 같다고 구현
+                .host(host)
+                .place(place)
+                .seatXBound(seatXBound)
+                .seatYBound(seatYBound)
+                .build();
+
+        Event savedEvent = eventRepository.save(event);
+        return savedEvent.getId();
+    }
+}
+
