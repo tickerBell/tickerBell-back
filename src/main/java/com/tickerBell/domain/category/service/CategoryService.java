779af6d27d8e29b08c70b8a63a7fe3f5bcbbf34a
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-09-29 15:26:59 +0900

    Feat: saveCategory 메서드 구현

--- src/main/java/com/tickerBell/domain/category/service/CategoryService.java
+++ src/main/java/com/tickerBell/domain/category/service/CategoryService.java
@@ -1,4 +1,8 @@
 package com.tickerBell.domain.category.service;
 
+import com.tickerBell.domain.category.entity.Categories;
+
 public interface CategoryService {
+
+    Long saveCategory(Long eventId, Categories categories);
 }
