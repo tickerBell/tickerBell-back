5c2c642fd07bed5a0b68250b0da55c3da11012e7
dgjinsu <rlawlstn3206@naver.com>
2023-10-01 16:49:56 +0900

    docs: example 데이터 변경

--- src/main/java/com/tickerBell/domain/member/dtos/LoginRequest.java
+++ src/main/java/com/tickerBell/domain/member/dtos/LoginRequest.java
@@ -13,11 +13,11 @@ public class LoginRequest {
 
     @NotEmpty
     @Size(min = 2)
-    @Schema(description = "사용자 Id", example = "username")
+    @Schema(description = "사용자 Id", example = "abcdefg")
     private String username;
     @NotEmpty
     @Size(min = 6)
-    @Schema(description = "사용자 비밀번호", example = "pass12!")
+    @Schema(description = "사용자 비밀번호", example = "abcdefg1")
     @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-zA-Z]).*$", message = "비밀번호는 숫자와 영문자를 모두 포함해야 합니다.")
     private String password;
 }
