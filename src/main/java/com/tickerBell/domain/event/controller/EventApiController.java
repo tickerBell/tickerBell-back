9c6e4d28c106b768c8817c1ef76a46088739f4ef
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-10-01 13:16:42 +0900

    Feat: 이벤트 등록 시 태그 저장 로직 추가

--- src/main/java/com/tickerBell/domain/event/controller/EventApiController.java
+++ src/main/java/com/tickerBell/domain/event/controller/EventApiController.java
@@ -6,6 +6,7 @@ import com.tickerBell.domain.event.dtos.SaveEventRequest;
 import com.tickerBell.domain.event.service.EventService;
 import com.tickerBell.domain.member.entity.Member;
 import com.tickerBell.domain.specialseat.service.SpecialSeatService;
+import com.tickerBell.domain.tag.service.TagService;
 import com.tickerBell.global.dto.Response;
 import com.tickerBell.global.security.context.MemberContext;
 import lombok.RequiredArgsConstructor;
@@ -26,12 +27,15 @@ public class EventApiController {
     private final EventService eventService;
     private final CategoryService categoryService;
     private final SpecialSeatService specialSeatService;
+    private final TagService tagService;
 
     @PostMapping("/api/event")
     public ResponseEntity<Response> saveEvent(@RequestBody SaveEventRequest request,
                                               @AuthenticationPrincipal MemberContext memberContext) {
 
+        // 로그인한 회원 객체 조회
         Member loginMember = memberContext.getMember();
+        // 이벤트 저장
         Long savedEventId = eventService.saveEvent(
                 loginMember.getId(),
                 request.getName(),
@@ -45,12 +49,20 @@ public class EventApiController {
                 request.getPlace(),
                 request.getAge());
 
+        // 카테고리 저장
         List<Categories> categories = request.getCategories();
         for (Categories category : categories) {
             categoryService.saveCategory(savedEventId, category);
         }
 
+        // 특수석 저장
         specialSeatService.saveSpecialSeat(savedEventId, request.getIsSpecialA(), request.getIsSpecialB(), request.getIsSpecialC());
+
+        // 태그 저장
+        List<String> tags = request.getTags();
+        for (String tag : tags) {
+            tagService.saveTag(tag, savedEventId, loginMember.getId());
+        }
 
         return ResponseEntity.ok(new Response("이벤트 등록에 성공하였습니다."));
     }
