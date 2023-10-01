715bc9704c4e29774aefcc0261cf89a5ab17cfe3
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-10-01 20:18:29 +0900

    Delete: 카테고리 정보를 미리 DB에 저장하던 로직 삭제

--- src/main/java/com/tickerBell/global/InitDB.java
+++ src/main/java/com/tickerBell/global/InitDB.java
@@ -1,7 +1,5 @@
 package com.tickerBell.global;
 
-import com.tickerBell.domain.category.entity.Categories;
-import com.tickerBell.domain.category.entity.Category;
 import com.tickerBell.domain.member.entity.AuthProvider;
 import com.tickerBell.domain.member.entity.Member;
 import com.tickerBell.domain.member.entity.Role;
@@ -9,7 +7,6 @@ import jakarta.annotation.PostConstruct;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -25,7 +22,6 @@ public class InitDB {
 
     @PostConstruct
     public void init() {
-        initService.initCategory();
         initService.initMember();
     }
 
@@ -35,20 +31,6 @@ public class InitDB {
     public static class InitService {
         private final EntityManager em;
         private final BCryptPasswordEncoder encoder;
-
-        public void initCategory() {
-            Category category1 = Category.builder().name(Categories.MUSICAL.getDescription()).build();
-            Category category2 = Category.builder().name(Categories.CONCERT.getDescription()).build();
-            Category category3 = Category.builder().name(Categories.PLAY.getDescription()).build();
-            Category category4 = Category.builder().name(Categories.CLASSIC.getDescription()).build();
-            Category category5 = Category.builder().name(Categories.SPORTS.getDescription()).build();
-
-            em.persist(category1);
-            em.persist(category2);
-            em.persist(category3);
-            em.persist(category4);
-            em.persist(category5);
-        }
 
         public void initMember() {
             Member member1 = Member.builder().username("abcdefg").password(encoder.encode("abcdefg1")).phone("01012345678").email("email@gmail.com")
