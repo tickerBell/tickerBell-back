517683111699889cf59fc28b305a3a3037876eba
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-10-01 20:20:44 +0900

    Refactor: 카테고리 저장 방식 변환으로 인한 서비스 계층 변경

--- src/main/java/com/tickerBell/domain/event/service/EventService.java
+++ src/main/java/com/tickerBell/domain/event/service/EventService.java
@@ -1,11 +1,12 @@
 package com.tickerBell.domain.event.service;
 
 import com.tickerBell.domain.event.dtos.EventListResponse;
+import com.tickerBell.domain.event.entity.Category;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface EventService {
-    Long saveEvent(Long memberId, String name, LocalDateTime startEvent, LocalDateTime endEvent, Integer normalPrice, Integer premiumPrice, Float saleDegree, String casting, String host, String place, Integer age, String categoryName);
+    Long saveEvent(Long memberId, String name, LocalDateTime startEvent, LocalDateTime endEvent, Integer normalPrice, Integer premiumPrice, Float saleDegree, String casting, String host, String place, Integer age, Category category);
     List<EventListResponse> getEventByCategory(String categoryName);
 }
--- src/main/java/com/tickerBell/domain/event/service/EventServiceImpl.java
+++ src/main/java/com/tickerBell/domain/event/service/EventServiceImpl.java
@@ -1,8 +1,7 @@
 package com.tickerBell.domain.event.service;
 
-import com.tickerBell.domain.category.entity.Category;
-import com.tickerBell.domain.category.repository.CategoryRepository;
 import com.tickerBell.domain.event.dtos.EventListResponse;
+import com.tickerBell.domain.event.entity.Category;
 import com.tickerBell.domain.event.entity.Event;
 import com.tickerBell.domain.event.repository.EventRepository;
 import com.tickerBell.domain.member.entity.Member;
@@ -16,7 +15,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -27,21 +25,14 @@ public class EventServiceImpl implements EventService {
     private final Integer TOTALSEAT = 60; // 총 좌석 수 60으로 고정
     private final EventRepository eventRepository;
     private final MemberRepository memberRepository;
-    private final CategoryRepository categoryRepository;
 
     @Override
     @Transactional
-    public Long saveEvent(Long memberId, String name, LocalDateTime startEvent, LocalDateTime endEvent, Integer normalPrice, Integer premiumPrice, Float saleDegree, String casting, String host, String place, Integer age, String categoryName) {
+    public Long saveEvent(Long memberId, String name, LocalDateTime startEvent, LocalDateTime endEvent, Integer normalPrice, Integer premiumPrice, Float saleDegree, String casting, String host, String place, Integer age, Category category) {
 
         Member findMember = memberRepository.findById(memberId).orElseThrow(
                 () -> new CustomException(ErrorCode.MEMBER_NOT_FOUND)
         );
-
-        // [category] 추가
-        // 유저가 선택한 category db에서 조회
-        Category category = categoryRepository.findByName(categoryName)
-                .orElseThrow(() -> new CustomException(ErrorCode.CATEGORY_NOT_FOUND));
-
 
         Event event = Event.builder()
                 .name(name)
@@ -65,12 +56,7 @@ public class EventServiceImpl implements EventService {
 
     @Override
     public List<EventListResponse> getEventByCategory(String categoryName) {
-        Category category = categoryRepository.findByName(categoryName)
-                .orElseThrow(() -> new CustomException(ErrorCode.CATEGORY_NOT_FOUND));
-        List<EventListResponse> eventListResponseList = eventRepository.findByCategoryName(category.getId()).stream()
-                .map(event -> EventListResponse.from(event))
-                .collect(Collectors.toList());
-        return eventListResponseList;
+        return null;
     }
 }
 
