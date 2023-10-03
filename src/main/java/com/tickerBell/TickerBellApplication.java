09d882b653dfe4d184d2e4d7fbe99213ad0087df
dgjinsu <rlawlstn3206@naver.com>
2023-10-03 22:48:15 +0900

    feat: Asia/Seoul timezone 설정

--- src/main/java/com/tickerBell/TickerBellApplication.java
+++ src/main/java/com/tickerBell/TickerBellApplication.java
@@ -1,15 +1,26 @@
 package com.tickerBell;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableScheduling
 public class TickerBellApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TickerBellApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init() {
+		// timezone 설정
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 	}
 
 }
