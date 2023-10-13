53d6427ad93d9ae04eee1fbe143741a13bfea2fd
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-10-13 15:11:33 +0900

    Feat: 이벤트 PK 로 캐스팅 조회 구현

--- src/main/java/com/tickerBell/domain/casting/repository/CastingRepository.java
+++ src/main/java/com/tickerBell/domain/casting/repository/CastingRepository.java
@@ -2,6 +2,11 @@ package com.tickerBell.domain.casting.repository;
 
 import com.tickerBell.domain.casting.entity.Casting;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CastingRepository extends JpaRepository<Casting, Long> {
+
+    List<Casting> findByEventId(@Param(value = "eventId") Long eventId);
 }
