4c766311b067b11ab29adc07170ac91b9161c0f8
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-10-13 15:11:49 +0900

    Test: 이벤트 PK 로 캐스팅 조회 테스트 구현

--- src/test/java/com/tickerBell/domain/casting/repository/CastingRepositoryTest.java
+++ src/test/java/com/tickerBell/domain/casting/repository/CastingRepositoryTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
@@ -33,5 +35,23 @@ class CastingRepositoryTest {
         assertThat(savedCasting.getId()).isEqualTo(casting.getId());
         assertThat(savedCasting.getCastingName()).isEqualTo(casting.getCastingName());
         assertThat(savedCasting.getEvent()).isEqualTo(casting.getEvent());
+    }
+
+    @Test
+    void findByEventIdTest() {
+        // given
+        Event event = Event.builder().build();
+        Event savedEvent = eventRepository.save(event);
+        String castingName = "mockCasting";
+        Casting casting = Casting.builder().castingName(castingName).event(savedEvent).build();
+        castingRepository.save(casting);
+        Casting casting2 = Casting.builder().castingName(castingName + "2").event(savedEvent).build();
+        castingRepository.save(casting2);
+
+        // when
+        List<Casting> findCastings = castingRepository.findByEventId(savedEvent.getId());
+
+        // then
+        assertThat(findCastings.size()).isEqualTo(2);
     }
 }
