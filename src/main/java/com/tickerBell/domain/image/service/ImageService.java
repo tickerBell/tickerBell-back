f2371dff9e3a3628b6cde738f4b516cc74cf1042
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-10-15 20:45:16 +0900

    Refactor: uploadImage 내부 예외처리로 변경

--- src/main/java/com/tickerBell/domain/image/service/ImageService.java
+++ src/main/java/com/tickerBell/domain/image/service/ImageService.java
@@ -1,15 +1,13 @@
 package com.tickerBell.domain.image.service;
 
-import com.tickerBell.domain.image.dtos.ImageRequest;
 import com.tickerBell.domain.image.dtos.ImageResponse;
 import com.tickerBell.domain.image.entity.Image;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.util.List;
 
 public interface ImageService {
-    List<Image> uploadImage(MultipartFile multipartFile, List<MultipartFile> multipartFiles) throws IOException;
+    List<Image> uploadImage(MultipartFile multipartFile, List<MultipartFile> multipartFiles);
 
     List<ImageResponse> findAllImage();
     void deleteImage(List<Image> imageList);
