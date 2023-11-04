96ce48e8d1a2fde0a70231a21f851a4cf71ac9c8
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-11-04 14:23:50 +0900

    Feat: SSE 구독 API 개발

---
+++ src/main/java/com/tickerBell/domain/emitter/controller/EmitterApiController.java
@@ -0,0 +1,29 @@
+package com.tickerBell.domain.emitter.controller;
+
+import com.tickerBell.domain.emitter.service.EmitterService;
+import com.tickerBell.domain.member.entity.Member;
+import com.tickerBell.global.security.context.MemberContext;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class EmitterApiController {
+
+    private final EmitterService emitterService;
+
+    @GetMapping(value = "/api/emitter/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public SseEmitter subscribe (@RequestHeader(value = "Last-Event-Id", required = false, defaultValue = " ") String lastEventId,
+                                 @AuthenticationPrincipal MemberContext memberContext) {
+
+        Member loginMember = memberContext.getMember();
+        return emitterService.subscribe(loginMember.getId());
+    }
+}
