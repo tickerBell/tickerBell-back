f136c4ba6dbe78ba5f8dff7506b3691d83e8b470
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-11-05 15:45:23 +0900

    Feat: s3Url 을 이용한 이미지 조회 쿼리 추가

--- src/main/java/com/tickerBell/domain/image/repository/ImageRepository.java
+++ src/main/java/com/tickerBell/domain/image/repository/ImageRepository.java
@@ -7,10 +7,13 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ImageRepository extends JpaRepository<Image, Long> {
     // eventId 에 해당하는 image 조회
     @Query("select i from Image i where i.event.id = :eventId")
     List<Image> findImageByEventId(@Param("eventId") Long eventId);
+    @Query("select i from Image i where i.s3Url = :imageUrl")
+    Optional<Image> findByImageUrl(@Param("imageUrl") String imageUrl);
 }
