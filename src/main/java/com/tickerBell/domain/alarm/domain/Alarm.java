9e18c71e0e31e04ae5eaac3d699888e8d37b3e94
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-11-05 17:12:54 +0900

    Feat: 알람 엔티티 AlarmType 삭제

--- src/main/java/com/tickerBell/domain/alarm/domain/Alarm.java
+++ src/main/java/com/tickerBell/domain/alarm/domain/Alarm.java
@@ -21,19 +21,15 @@ public class Alarm extends BaseEntity {
     private String message; // 알림 메시지
     private Boolean isRead; // 읽음 여부
 
-    @Enumerated(EnumType.STRING)
-    private AlarmType alarmType;
-
     //== 연관 관계 ==//
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member; // 알람, 회원 N : 1 다대일 단방향 매핑
 
     @Builder
-    public Alarm(String message, Boolean isRead, AlarmType alarmType, Member member) {
+    public Alarm(String message, Boolean isRead, Member member) {
         this.message = message;
         this.isRead = isRead;
-        this.alarmType = alarmType;
         this.member = member;
     }
 }
