2fd6b3f96b82f027436bc15b23fec7a25316f9c8
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-11-05 15:56:41 +0900

    Feat: 이벤트 등록 시 이미지 엔티티에 이벤트 연관관계 설정

--- src/main/java/com/tickerBell/domain/event/service/EventService.java
+++ src/main/java/com/tickerBell/domain/event/service/EventService.java
@@ -5,12 +5,11 @@ import com.tickerBell.domain.event.dtos.EventResponse;
 import com.tickerBell.domain.event.dtos.MainPageDto;
 import com.tickerBell.domain.event.dtos.SaveEventRequest;
 import com.tickerBell.domain.event.entity.Category;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 public interface EventService {
-    Long saveEvent(Long memberId, SaveEventRequest request, MultipartFile thumbNailImage, List<MultipartFile> eventImages);
+    Long saveEvent(Long memberId, SaveEventRequest request);
     List<EventListResponse> getEventByCategory(Category category);
     EventResponse findByIdFetchAll(Long eventId);
     MainPageDto getMainPage();
