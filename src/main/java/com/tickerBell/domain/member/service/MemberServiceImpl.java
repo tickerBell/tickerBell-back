8bff60eacaa401e442667b7f6ed769e42f69a0d0
dgjinsu <rlawlstn3206@naver.com>
2023-11-06 19:49:21 +0900

    refactor: 오탈자수정

--- src/main/java/com/tickerBell/domain/member/service/MemberServiceImpl.java
+++ src/main/java/com/tickerBell/domain/member/service/MemberServiceImpl.java
@@ -95,7 +95,6 @@ public class MemberServiceImpl implements MemberService {
         return loginResponse;
     }
 
-}
 
     @Override
     public LoginResponse login(String username, String password) {
