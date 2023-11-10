73f401240506d9f1345c37f18d8ea8223f1bbe71
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-11-10 11:12:35 +0900

    Test: 서비스 레이어 알람 저장 실패 테스트 개발

--- src/test/java/com/tickerBell/domain/alarm/service/AlarmServiceTest.java
+++ src/test/java/com/tickerBell/domain/alarm/service/AlarmServiceTest.java
@@ -5,6 +5,9 @@ import com.tickerBell.domain.alarm.dtos.SaveAlarmRequest;
 import com.tickerBell.domain.alarm.repository.AlarmRepository;
 import com.tickerBell.domain.member.entity.Member;
 import com.tickerBell.domain.member.repository.MemberRepository;
+import com.tickerBell.global.exception.CustomException;
+import com.tickerBell.global.exception.ErrorCode;
+import org.assertj.core.api.AbstractObjectAssert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,6 +17,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -47,5 +51,32 @@ public class AlarmServiceTest {
         // then
         verify(memberRepository, times(1)).findById(memberId);
         verify(alarmRepository, times(1)).save(any(Alarm.class));
+    }
+
+    @Test
+    @DisplayName("알람 저장 실패 테스트")
+    void saveAlarmFailTest() {
+        // given
+        Long memberId = 1L;
+        String message = "message";
+        SaveAlarmRequest saveAlarmRequest = new SaveAlarmRequest();
+        saveAlarmRequest.setMemberId(memberId);
+        saveAlarmRequest.setMessage(message);
+
+        // stub
+        when(memberRepository.findById(memberId)).thenReturn(Optional.empty());
+
+        // when
+        AbstractObjectAssert<?, CustomException> extracting = assertThatThrownBy(() -> alarmService.saveAlarm(saveAlarmRequest))
+                .isInstanceOf(CustomException.class)
+                .extracting(ex -> (CustomException) ex);
+
+        // then
+        extracting.satisfies(ex -> {
+            assertThat(ex.getErrorCode()).isEqualTo(ErrorCode.MEMBER_NOT_FOUND);
+        });
+        verify(memberRepository, times(1)).findById(memberId);
+        verifyNoInteractions(alarmRepository);
+
     }
 }
