48d408b63a71afdee61bd1bf473643b198ea3f25
dgjinsu <rlawlstn3206@naver.com>
2023-11-26 19:52:02 +0900

    docs: swagger summary 변경

--- src/main/java/com/tickerBell/domain/ticketing/controller/TicketingController.java
+++ src/main/java/com/tickerBell/domain/ticketing/controller/TicketingController.java
@@ -26,7 +26,7 @@ public class TicketingController {
 
     private final TicketingService ticketingService;
 
-    @Operation(summary = "이벤트 회원 예매", description = "회원일 때 이벤트 예매")
+    @Operation(summary = "회원:  이벤트 예매", description = "회원일 때 이벤트 예매")
     @PostMapping("/ticketing")
         public ResponseEntity<Response> ticketingEvent(@AuthenticationPrincipal MemberContext memberContext,
                                                    @RequestBody @Valid TicketingRequest request) {
@@ -37,14 +37,14 @@ public class TicketingController {
         return ResponseEntity.ok(new Response("회원 event 예매 완료"));
     }
 
-    @Operation(summary = "이벤트 비회원 예매", description = "비회원일 때 이벤트 예매")
+    @Operation(summary = "비회원:  이벤트 예매", description = "비회원일 때 이벤트 예매")
     @PostMapping("/ticketing-nonMember")
     public ResponseEntity<Response> ticketingEventNonMember(@RequestBody @Valid TicketingNonMemberRequest request) {
         ticketingService.saveTicketingNonMember(request);
         return ResponseEntity.ok(new Response("비회원 event 예매 완료"));
     }
 
-    @Operation(summary = "예매 내역 회원 조회", description = "회원일 때 이벤트 조회")
+    @Operation(summary = "회원:  예매 내역 조회", description = "회원일 때 이벤트 조회")
     @GetMapping("/ticketing")
     public ResponseEntity<Response> ticketingHistory(@AuthenticationPrincipal MemberContext memberContext,
                                                      @RequestParam(name = "page", required = false, defaultValue = "0") int page,
@@ -53,7 +53,7 @@ public class TicketingController {
                 ticketingService.getTicketingHistory(memberContext.getMember().getId(), page, size);
         return ResponseEntity.ok(new Response(ticketingResponseList, "회원 예매 정보 반환"));
     }
-    @Operation(summary = "예매 내역 비회원 조회", description = "비회원일 때 이벤트 조회")
+    @Operation(summary = "비회원:  예매 내역 조회", description = "비회원일 때 이벤트 조회")
     @GetMapping("/ticketing-nonMember")
     public ResponseEntity<Response> ticketingHistoryNonMember(@RequestParam("name") String name,
                                                               @RequestParam("phone") String phone,
@@ -64,7 +64,7 @@ public class TicketingController {
         return ResponseEntity.ok(new Response(ticketingResponseList, "비회원 예매 정보 반환"));
     }
 
-    @Operation(summary = "회원 예매 취소", description = "회원일 때 예매 취소")
+    @Operation(summary = "회원:  예매 취소", description = "회원일 때 예매 취소")
     @DeleteMapping("/ticketing/{ticketingId}")
     public ResponseEntity<Response> ticketingCancelMember(@AuthenticationPrincipal MemberContext memberContext,
                                                           @PathVariable("ticketingId") Long ticketingId) {
@@ -72,7 +72,7 @@ public class TicketingController {
         return ResponseEntity.ok(new Response("회원 예매 내역 취소 완료"));
     }
 
-    @Operation(summary = "비회원 예매 취소", description = "비회원일 때 예매 취소")
+    @Operation(summary = "비회원:  예매 취소", description = "비회원일 때 예매 취소")
     @DeleteMapping("/ticketing-nonMember/{ticketingId}")
     public ResponseEntity<Response> ticketingCancelNonMember(@RequestBody TicketingNonMemberCancelRequest request,
                                                              @PathVariable("ticketingId") Long ticketingId) {
