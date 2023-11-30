b09f37c950a952fbb137edbc4bfb1d648f3dbe8a
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-11-30 17:05:43 +0900

    Test: JwtFilter 실패 테스트 개발

--- src/test/java/com/tickerBell/global/security/filter/JwtFilterTest.java
+++ src/test/java/com/tickerBell/global/security/filter/JwtFilterTest.java
@@ -14,6 +14,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
@@ -21,6 +22,7 @@ import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -58,5 +60,47 @@ public class JwtFilterTest {
         // then
         verify(jwtTokenProvider, times(1)).isExpiration(any(String.class));
         verify(memberRepository, times(1)).findByUsername("username");
+    }
+
+    @Test
+    @DisplayName("필터 토큰만료 테스트")
+    void doFilterInternalTokenFailTest() throws ServletException, IOException {
+        // given
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.addHeader("Authorization", "Bearer accessToken");
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        Claims claims = Mockito.mock(Claims.class);
+        FilterChain filterChain = Mockito.mock(FilterChain.class);
+
+        // stub
+        when(jwtTokenProvider.isExpiration(any(String.class))).thenReturn(true);
+
+        // when
+        jwtFilter.doFilterInternal(request, response, filterChain);
+
+        // then
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
+    }
+
+    @Test
+    @DisplayName("필터 회원조회 실패 테스트")
+    void doFilterInternalMemberFailTest() throws ServletException, IOException {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.addHeader("Authorization", "Bearer accessToken");
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        Claims claims = Mockito.mock(Claims.class);
+        FilterChain filterChain = Mockito.mock(FilterChain.class);
+
+        // stub
+        when(jwtTokenProvider.isExpiration(any(String.class))).thenReturn(false);
+        when(jwtTokenProvider.get(any(String.class))).thenReturn(claims);
+        when(jwtTokenProvider.get("accessToken").get("username")).thenReturn("username");
+        when(memberRepository.findByUsername("username")).thenReturn(Optional.empty());
+
+        // when
+        jwtFilter.doFilterInternal(request, response, filterChain);
+
+        // then
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
     }
 }
