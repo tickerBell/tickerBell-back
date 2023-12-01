a19d6492a5a9384a2ae506934f8794d56239749c
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-12-01 15:59:47 +0900

    Feat: sse 서비스 로깅

--- src/main/java/com/tickerBell/domain/emitter/service/EmitterServiceImpl.java
+++ src/main/java/com/tickerBell/domain/emitter/service/EmitterServiceImpl.java
@@ -2,11 +2,13 @@ package com.tickerBell.domain.emitter.service;
 
 import com.tickerBell.domain.emitter.repository.EmitterRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class EmitterServiceImpl implements EmitterService{
@@ -31,10 +33,12 @@ public class EmitterServiceImpl implements EmitterService{
         SseEmitter sseEmitter = new SseEmitter(DEFAULT_TIMEOUT);
         emitterRepository.save(memberId, sseEmitter);
 
+        log.info("in createEmitter");
         // Emitter가 완료될 때(모든 데이터가 성공적으로 전송된 상태) Emitter를 삭제한다.
         sseEmitter.onCompletion(() -> emitterRepository.deleteById(memberId));
         // Emitter가 타임아웃 되었을 때(지정된 시간동안 어떠한 이벤트도 전송되지 않았을 때) Emitter를 삭제한다.
         sseEmitter.onTimeout(() -> emitterRepository.deleteById(memberId));
+        log.info("in createEmitterAfter");
 
         return sseEmitter;
     }
@@ -44,7 +48,9 @@ public class EmitterServiceImpl implements EmitterService{
 
         if (findSseEmitter != null) {
             try {
+                log.info("in sendToClient");
                 findSseEmitter.send(SseEmitter.event().id(String.valueOf(memberId)).name("sse").data(data));
+                log.info("in sendToClientSendAfter");
             } catch (IOException e) {
                 emitterRepository.deleteById(memberId);
                 findSseEmitter.completeWithError(e);
