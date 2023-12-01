5b42919e39d4880c2fa53d4381d465131258b84a
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-12-01 16:58:17 +0900

    Feat: sse 서비스 로깅 삭제

--- src/main/java/com/tickerBell/domain/emitter/service/EmitterServiceImpl.java
+++ src/main/java/com/tickerBell/domain/emitter/service/EmitterServiceImpl.java
@@ -33,20 +33,14 @@ public class EmitterServiceImpl implements EmitterService{
         SseEmitter sseEmitter = new SseEmitter(DEFAULT_TIMEOUT);
         emitterRepository.save(memberId, sseEmitter);
 
-        log.info("in createEmitter");
         // Emitter가 완료될 때(모든 데이터가 성공적으로 전송된 상태) Emitter를 삭제한다.
         sseEmitter.onCompletion(() -> {
-            log.info("onCompletion");
             emitterRepository.deleteById(memberId);
-            log.info("onCompletionAfter");
         });
         // Emitter가 타임아웃 되었을 때(지정된 시간동안 어떠한 이벤트도 전송되지 않았을 때) Emitter를 삭제한다.
         sseEmitter.onTimeout(() -> {
-            log.info("onTimeout");
             emitterRepository.deleteById(memberId);
-            log.info("onTimeoutAfter");
         });
-        log.info("in createEmitterAfter");
 
         return sseEmitter;
     }
@@ -56,9 +50,7 @@ public class EmitterServiceImpl implements EmitterService{
 
         if (findSseEmitter != null) {
             try {
-                log.info("in sendToClient");
                 findSseEmitter.send(SseEmitter.event().id(String.valueOf(memberId)).name("sse").data(data));
-                log.info("in sendToClientSendAfter");
             } catch (IOException e) {
                 emitterRepository.deleteById(memberId);
                 findSseEmitter.completeWithError(e);
