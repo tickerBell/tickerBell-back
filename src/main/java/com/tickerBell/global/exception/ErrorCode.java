1ba0e4e58bcbc4d971578f836b3e2d90ae0a4c11
dgjinsu <rlawlstn3206@naver.com>
2023-12-03 16:35:41 +0900

    refactor: NO_CONTENT 에러 -> NOT_FOUND 에러로 변경

--- src/main/java/com/tickerBell/global/exception/ErrorCode.java
+++ src/main/java/com/tickerBell/global/exception/ErrorCode.java
@@ -8,8 +8,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ErrorCode {
 
-    MEMBER_NOT_FOUND(HttpStatus.NO_CONTENT, "회원이 존재하지 않습니다."),
-    NON_MEMBER_NOT_FOUND(HttpStatus.NO_CONTENT, "비회원 정보가 존재하지 않습니다."),
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "회원이 존재하지 않습니다."),
+    NON_MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "비회원 정보가 존재하지 않습니다."),
     MEMBER_ALREADY_EXIST(HttpStatus.BAD_REQUEST, "이미 존재하는 아이디입니다."),
     PASSWORD_NOT_MATCH(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
 
@@ -22,17 +22,17 @@ public enum ErrorCode {
 
     REQUEST_INVALID(HttpStatus.BAD_REQUEST, "valid 옵션에 맞지 않는 형식입니다."),
     IMAGE_NOT_SUPPORTED_EXTENSION(HttpStatus.BAD_REQUEST, "지원하지 않는 이미지 형식 입니다. jpg, png 형식으로 보내주세요."),
-    IMAGE_NOT_FOUND(HttpStatus.NO_CONTENT, "저장되지 않은 이미지입니다."),
+    IMAGE_NOT_FOUND(HttpStatus.NOT_FOUND, "저장되지 않은 이미지입니다."),
     IMAGE_NOT_FOUND_EXTENSION(HttpStatus.BAD_REQUEST, "확장자를 찾을 수 없습니다."),
     IMAGE_EXCEPTION(HttpStatus.BAD_REQUEST, "이미지 처리 중 에러 발생"),
-    EVENT_NOT_FOUND(HttpStatus.NO_CONTENT, "등록되지 않은 Event 입니다."),
+    EVENT_NOT_FOUND(HttpStatus.NOT_FOUND, "등록되지 않은 Event 입니다."),
     EVENT_CANCEL_FAIL(HttpStatus.BAD_REQUEST, "이벤트 취소에 실패하였습니다."),
-    CATEGORY_NOT_FOUND(HttpStatus.NO_CONTENT, "등록되지 않은 Category 입니다."),
+    CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "등록되지 않은 Category 입니다."),
     SEAT_INFO_NOT_VALID_FORMAT(HttpStatus.BAD_REQUEST, "선택 좌석의 형식이 올바르지 않습니다. A-1, B-1 형식으로 보내주세요"),
     SALE_DEGREE_NOT_VALID_FORMAT(HttpStatus.BAD_REQUEST, "세일 degree 형식이 올바르지 않습니다."),
     ALREADY_SELECTED_SEAT(HttpStatus.CONFLICT, "이미 선택된 좌석입니다. 다른 좌석을 선택해주세요"), // 409: 해당 요청의 처리 중 비지니스 로직상 불가능하거나 모순이 생긴 경우
     SMS_SEND_FAIL(HttpStatus.INTERNAL_SERVER_ERROR, "sms 문자 전송 중 에러가 발생했습니다."),
-    TICKETING_NOT_FOUND(HttpStatus.NO_CONTENT, "예매내역이 존재하지 않습니다."),
+    TICKETING_NOT_FOUND(HttpStatus.NOT_FOUND, "예매내역이 존재하지 않습니다."),
     TICKETING_DELETE_FAIL(HttpStatus.INTERNAL_SERVER_ERROR, "티켓 삭제 중 에러가 발생했습니다."),
     MAP_API_CALL_FAIL(HttpStatus.INTERNAL_SERVER_ERROR, "naver map api 호출 중 에러가 발생했습니다."),
     SELECTED_DATE_INVALID(HttpStatus.BAD_REQUEST, "선택된 날짜가 이벤트 시작 종료 날짜에 벗어났거나 아직 구매 가능한 시간이 아닙니다."),
